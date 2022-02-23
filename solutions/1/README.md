1f0a60f0cc55cc42f00dde9417cf6f9a23a09400
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2022-02-23 19:17:45 +0000

    Improve javascript solution of the problem #1, readme of the same problem moved to the main folder

--- solutions/1/README.md
+++ solutions/1/README.md
@@ -3,3 +3,4 @@
 |  Time Submitted  |  Status  | Runtime | Memory  |  Language  |
 | :--------------: | :------: | :-----: | :-----: | :--------: |
 | 02/23/2022 17:10 | Accepted | 113 ms  | 42.4 MB | javascript |
+| 02/23/2022 19:08 | Accepted |  73 ms  | 43.1 MB | javascript |
--- solutions/1/javascript/solution.js
+++ solutions/1/javascript/solution.js
@@ -1,9 +1,22 @@
 // Should return indices of the two numbers such that they add up to target.
-const twoSum = (nums, target) => {
+
+// Old version - the problem is that, this solution has a complexity of O(n^2)
+/* const twoSum = (nums, target) => {
 	for (let i = 0; i < nums.length; i++) {
 		let aux = target - nums[i];
 		for (let j = i + 1; j < nums.length; j++) {
 			if (nums[j] === aux) return [i, j];
 		}
+	}
+}; */
+
+// Better version - with a complexity of O(n)
+const twoSum = (nums, target) => {
+	const map = new Map();
+
+	for (let i = 0; i < nums.length; i++) {
+		let requiredNum = target - nums[i];
+		if (map.has(requiredNum)) return [map.get(requiredNum), i];
+		map.set(nums[i], i);
 	}
 };
